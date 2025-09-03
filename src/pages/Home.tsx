69a9c649370961e929dfe9b6deddd55fd614a241
safin33221 <safin33221@gmail.com>
2025-09-04 01:16:22 +0600

    Completed How Work it section in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Features from "@/components/Modules/Home/Features";
 import { Hero } from "@/components/Modules/Home/Hero";
+import HowItWorks from "@/components/Modules/Home/HowItWorks";
 
 export default function Home() {
   return (
     <div className="space-y-20">
       <Hero />
       <Features />
+      <HowItWorks/>
     </div>
   );
 };
