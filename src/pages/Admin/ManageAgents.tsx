39899fc3df64b74559932c2bcf101a08b58bb43b
safin33221 <safin33221@gmail.com>
2025-09-07 15:53:33 +0600

    Implement filtering at user and agent page

--- src/pages/Admin/ManageAgents.tsx
+++ src/pages/Admin/ManageAgents.tsx
@@ -1,12 +1,25 @@
 import { ViewUserDetailsModal } from "@/components/Modules/Admin/ViewUserDetailsModal";
 import { Button } from "@/components/ui/button";
-import { role } from "@/constants/Role";
 import { useGetAllUsersQuery, useUpdateUserMutation } from "@/redux/features/user/user.api";
 import type { IUser } from "@/types/user.type";
+import { useState } from "react";
 import { toast } from "sonner";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 
 export default function ManageAgents() {
-  const { data: users, isLoading } = useGetAllUsersQuery({ role: role.agent });
+
+  const [search, setSearch] = useState("")
+  const [statusFilter, setStatusFilter] = useState("all")
+
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const query = {
+    searchTerm: search || undefined,
+    approved: statusFilter !== "all" ? statusFilter === "Approved" ? true : false : undefined,
+    page: currentPage,
+    role: "AGENT"
+  };
+  const { data: users, isLoading } = useGetAllUsersQuery(query);
+  const totalPage: number = users?.data?.meta?.totalPage
 
   const [updateUser] = useUpdateUserMutation()
 
@@ -31,7 +44,29 @@ export default function ManageAgents() {
     <div className="p-6 space-y-6">
       {/* Header Controls */}
       <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
-        <h1 className="text-2xl font-bold">👥 Agent Management</h1>
+        <h1 className="text-2xl font-bold">Agent Management</h1>
+
+        <div className="flex flex-wrap items-center gap-3">
+          <input
+            type="text"
+            placeholder="🔍 Search by phone or type"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="px-3 py-2 rounded-lg border border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white"
+          />
+
+          <select
+            value={statusFilter}
+            onChange={(e) => setStatusFilter(e.target.value)}
+            className="px-3 py-2 rounded-lg border border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white"
+          >
+            <option value="all">All Status</option>
+            <option value="Approved">Approved</option>
+            <option value="Suspend">Suspend</option>
+
+          </select>
+
+        </div>
       </div>
 
       {/* User Table */}
@@ -60,7 +95,7 @@ export default function ManageAgents() {
                   ))}
                 </tr>
               ))
-              : users?.data?.map((user: IUser) => (
+              : users?.data?.data?.map((user: IUser) => (
                 <tr
                   key={user._id}
                   className="border-b  hover:bg-primary/10"
@@ -113,6 +148,28 @@ export default function ManageAgents() {
               ))}
           </tbody>
         </table>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem className={currentPage === 1 ? "opacity-35 cursor-no-drop" : ""}>
+              <PaginationPrevious onClick={() => currentPage != 1 && setCurrentPage((prev) => prev - 1)} />
+            </PaginationItem>
+
+            {
+              Array.from({ length: totalPage }, (_, id) => id + 1).map((page) => (
+                <PaginationItem>
+                  <PaginationLink isActive={currentPage === page}>{page}</PaginationLink>
+                </PaginationItem>
+              ))
+            }
+
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem className={currentPage === totalPage ? "opacity-35 cursor-no-drop" : ""}>
+              <PaginationNext onClick={() => currentPage != totalPage && setCurrentPage((prev) => prev + 1)} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
 
         {/* Empty State */}
         {!isLoading && users?.data?.length === 0 && (
--- src/pages/Admin/ManageUser.tsx
+++ src/pages/Admin/ManageUser.tsx
@@ -1,12 +1,26 @@
 import { ViewUserDetailsModal } from "@/components/Modules/Admin/ViewUserDetailsModal";
 import { Button } from "@/components/ui/button";
-import { role } from "@/constants/Role";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import { useGetAllUsersQuery, useUpdateUserMutation } from "@/redux/features/user/user.api";
 import type { IUser } from "@/types/user.type";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function ManageUser() {
-  const { data: users, isLoading } = useGetAllUsersQuery({ role: role.user });
+  const [search, setSearch] = useState("")
+  const [statusFilter, setStatusFilter] = useState("all")
+
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const query = {
+    searchTerm: search || undefined,
+    status: statusFilter !== "all" ? statusFilter : undefined,
+    page: currentPage,
+    role: "USER"
+  };
+
+
+  const { data: users, isLoading } = useGetAllUsersQuery(query);
+  const totalPage: number = users?.data?.meta?.totalPage
   const [updateUser] = useUpdateUserMutation()
   const handleUpdateUser = async (data: { id: string, status: string }) => {
     try {
@@ -29,7 +43,29 @@ export default function ManageUser() {
     <div className="p-6 space-y-6">
       {/* Header Controls */}
       <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
-        <h1 className="text-2xl font-bold">👥 User Management</h1>
+        <h1 className="text-2xl font-bold"> User Management </h1>
+
+        <div className="flex flex-wrap items-center gap-3">
+          <input
+            type="text"
+            placeholder="🔍 Search by phone or type"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="px-3 py-2 rounded-lg border border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white"
+          />
+
+          <select
+            value={statusFilter}
+            onChange={(e) => setStatusFilter(e.target.value)}
+            className="px-3 py-2 rounded-lg border border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white"
+          >
+            <option value="all">All Status</option>
+            <option value="UNBLOCK">Active</option>
+            <option value="BLOCKED">Blocked</option>
+
+          </select>
+
+        </div>
       </div>
 
       {/* User Table */}
@@ -58,7 +94,7 @@ export default function ManageUser() {
                   ))}
                 </tr>
               ))
-              : users?.data?.map((user: IUser) => (
+              : users?.data?.data?.map((user: IUser) => (
                 <tr
                   key={user._id}
                   className="border-b hover:bg-gray-50 dark:hover:bg-gray-700"
@@ -102,6 +138,29 @@ export default function ManageUser() {
               ))}
           </tbody>
         </table>
+
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem className={currentPage === 1 ? "opacity-35 cursor-no-drop" : ""}>
+              <PaginationPrevious onClick={() => currentPage != 1 && setCurrentPage((prev) => prev - 1)} />
+            </PaginationItem>
+
+            {
+              Array.from({ length: totalPage }, (_, id) => id + 1).map((page) => (
+                <PaginationItem>
+                  <PaginationLink isActive={currentPage === page}>{page}</PaginationLink>
+                </PaginationItem>
+              ))
+            }
+
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem className={currentPage === totalPage ? "opacity-35 cursor-no-drop" : ""}>
+              <PaginationNext onClick={() => currentPage != totalPage && setCurrentPage((prev) => prev + 1)} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
 
         {/* Empty State */}
         {!isLoading && users?.data?.length === 0 && (
